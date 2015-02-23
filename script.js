fdae3ff57ce98ed6fe1d68357e7cecfce8e41cc8
govnodev <govnodev@inbox.ru>
2015-02-24 00:11:30 +0400

    Update script.js

--- script.js
+++ script.js
@@ -38,6 +38,36 @@ var sl = [
     'Дяриндярин',
     'Канифоль'
 ];
+/* бля, нибейте, спиздил отсюда - https://github.com/dafrancis/BenedictCumberbatchChromeExtension */
+var firstnamelist = ["Bumblebee", "Bandersnatch", "Broccoli", "Rinkydink", "Bombadil", "Boilerdang",
+    "Bandicoot", "Fragglerock", "Muffintop", "Crumplesack", "Congleton", "Blubberdick",
+    "Buffalo", "Benadryl", "Butterfree", "Burberry", "Whippersnatch", "Buttermilk",
+    "Beezlebub", "Budapest", "Boilerdang", "Blubberwhale", "Bumberstump", "Bulbasaur",
+    "Cogglesnatch", "Liverswort", "Bodybuild", "Johnnycash", "Bendydick", "Burgerking",
+    "Bonaparte", "Hairycooch", "Bunsenburner", "Billiardball", "Bukkake", "Baseballmitt",
+    "Blubberbutt", "Baseballbat", "Rumblesack", "Barister", "Danglerack", "Rinkydink",
+    "Bombadil", "Honkytonk", "Billyray", "Bumbleshack", "Snorkeldink", "Anglerfish",
+    "Beetlejuice", "Bedlington", "Bandicoot", "Boobytrap", "Blenderdick", "Bentobox",
+    "Anallube", "Pallettown", "Wimbledon", "Hairyballs", "Buttercup", "Blasphemy",
+    "Syphilis", "Snorkeldink", "Brandenburg", "Barbituate", "Snozzlebert", "Tiddleywomp",
+    "Bouillabaisse", "Wellington", "Benetton", "Bendandsnap", "Timothy", "Brewery",
+    "Bentobox", "Brandybuck"
+];
+var lastnamelist = ["Coddleswort", "Curdlesnoot", "Calldispatch", "Humperdinck", "Rivendell",
+    "Cuttlefish", "Lingerie", "Vegemite", "Ampersand", "Cumberbund", "Candycrush",
+    "Clombyclomp", "Cragglethatch", "Nottinghill", "Cabbagepatch", "Camouflage",
+    "Creamsicle", "Curdlemilk", "Upperclass", "Frumblesnatch", "Crumplehorn",
+    "Talisman", "Candlestick", "Chesterfield", "Bumbersplat", "Scratchnsniff",
+    "Snugglesnatch", "Charizard", "Ballsacksnip", "Carrotstick", "Cumbercooch",
+    "Crackerjack", "Crucifix", "Cuckatoo", "Cockletit", "Collywog", "Gigglesnort",
+    "Capncrunch", "Covergirl", "Cumbersnatch", "Countryside", "Coggleswort",
+    "Splishnsplash", "Copperwire", "Animorph", "Curdledmilk", "Cheddarcheese",
+    "Cottagecheese", "Crumplehorn", "Snickersbar", "Banglesnatch", "Stinkyrash",
+    "Cameltoe", "Chickenbroth", "Concubine", "Candygram", "Moldyspore", "Chuckecheese",
+    "Cankersore", "Crimpysnitch", "Wafflesmack", "Chowderpants", "Toodlesnoot",
+    "Clavichord", "Cuckooclock", "Oxfordshire", "Cumbersome", "Chickenstrips",
+    "Battleship", "Commonwealth", "Cunningsnatch", "Custardbath", "Kryptonite"
+];
 var repl = function(textNode) {
     textNode.data = textNode.data.replace('Бенедикт', fl[Math.floor(Math.random() * fl.length)]);
     textNode.data = textNode.data.replace('Кембербетч', sl[Math.floor(Math.random() * sl.length)]);
@@ -46,6 +76,8 @@ var repl = function(textNode) {
     textNode.data = textNode.data.replace('Кембербэтч', sl[Math.floor(Math.random() * sl.length)]);
     textNode.data = textNode.data.replace('Кэмбербетч', sl[Math.floor(Math.random() * sl.length)]);
     textNode.data = textNode.data.replace('Кэмбербэтч', sl[Math.floor(Math.random() * sl.length)]);
+    textNode.data = textNode.data.replace('Benedict', firstnamelist[Math.floor(Math.random() * firstnamelist.length)]);
+    textNode.data = textNode.data.replace('Cumberbatch', lastnamelist[Math.floor(Math.random() * lastnamelist.length)]);
 
 }
 
